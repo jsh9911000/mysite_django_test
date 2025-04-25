72995dbe407efc192c2a476efd94f0ea23948366
jsh <jsh9911000@naver.com>
2025-04-26 02:37:41 +0900

    create DB connect and learn MTV pattern

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path 
 from . import views 
 
+
+app_name = "polls"  # 앱 이름 명시.
+
 urlpatterns = [
     # 최상위 앱 mysite의 urls.py로부터 전달받으면, path()에서 첫번째 매개변수로 파라미터를 설정하는데 "" 비어 있기 때문에, 127.0.0.1/polls/로 url은 전달되고 views.py의 index 함수가 실행이 된다.
-    path("", views.index, name="index")  
+    path("", views.index, name="index"),  
+    # ex) /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex) /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex) /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+
+from polls.models import Question
 
 # Create your views here. => 클라이언트의 요청(request)에 대해서 응답(response)해주는 함수 또는 클래스를 정의. 
 def index(request):
-    return HttpResponse("Hello World.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]   # Question 모델(DB)에서 해당 데이터를 5개만 가져온다.
+    context = {                                         # json 처럼 탬플릿에 보내줄 데이터를 설정.
+        "latest_question_list" : latest_question_list, 
+    }
+    return render(request, 'polls/index.html', context)  # 탬플릿을 리턴하면 데이터도 전송.
+
+def detail(request, question_id):
+    try:            # try 문.
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:         # except 문 => 데이터가 없다면 발생.
+        raise Http404("Qustion does not exist")     # 404에러 발생시키면서 메세지 띄움.
+    return render(request, 'polls/detail.html', {"question":question})  # 탬플릿을 리턴하면 데이터도 전송.
+
+def results(request, question_id):
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s."%question_id)
